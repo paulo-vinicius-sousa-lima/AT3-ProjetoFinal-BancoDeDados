94a517d3ba9c353c125b5252f5640e1646f285f4
Paulo Vinícius Sousa Lima <pauloviniciussousalima15@gmail.com>
2025-09-19 15:15:58 -0300

    arquivo e pacote .cli criado

---
+++ SistemaHospital/src/main/java/ucb/hospital/cli/SistemaHospital.java
@@ -0,0 +1,12 @@
+package ucb.hospital.cli;
+
+/**
+ *
+ * @author paulo
+ */
+public class SistemaHospital {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
