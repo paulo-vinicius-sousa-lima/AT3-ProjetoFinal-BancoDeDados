f8497792b0168ee819a27900c57790b9101472e2
Paulo Vinícius Sousa Lima <pauloviniciussousalima15@gmail.com>
2025-09-19 14:26:58 -0300

    pacotes e arquivo cli criados

--- AT3-ProjetoFinal-BancoDeDados/src/main/java/ucb/hospital/cli/AT3ProjetoFinalBancoDeDados.java
+++ AT3-ProjetoFinal-BancoDeDados/src/main/java/ucb/hospital/cli/AT3ProjetoFinalBancoDeDados.java
@@ -1,8 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package ucb.hospital.at3.projetofinal.bancodedados;
+package ucb.hospital.cli;
 
 /**
  *
