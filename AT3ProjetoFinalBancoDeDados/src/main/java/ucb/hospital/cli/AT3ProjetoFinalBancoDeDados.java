63971468080a94cc25fab11fe7b20228c3d449ea
Paulo Vinícius Sousa Lima <pauloviniciussousalima15@gmail.com>
2025-09-19 14:41:36 -0300

    arquivo cli criado

---
+++ AT3ProjetoFinalBancoDeDados/src/main/java/ucb/hospital/cli/AT3ProjetoFinalBancoDeDados.java
@@ -0,0 +1,12 @@
+package ucb.hospital.cli;
+
+/**
+ *
+ * @author paulo
+ */
+public class AT3ProjetoFinalBancoDeDados {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
